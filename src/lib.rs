6b56bab63c41fe125f816610a18590e18013fbee
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-12-27 20:40:44 +0100

    Update lib.rs to check fizzbuzz

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
 extern crate neon;
 
 use neon::vm::{Call, Result, JS, Module};
-use neon::value::String;
+use neon::value::{String, Integer};
+use neon::mem::Handle;
 
-fn hello(call: Call) -> JS<String> {
+fn fizzbuzz(call: Call) -> JS<String> {
     let scope = call.scope;
-    Ok(String::new(scope, "hello node").unwrap())
+    let input: u32 = try!(try!(call.arguments.require(scope, 0)).check::<Integer>());
+    if input == 0 {
+        Ok(input.to_string());
+    }
+    let result = if input % 15 == 0 {
+        "FizzBuzz".to_string()
+    } else if input % 5 == 0 {
+        "Buzz".to_string()
+    } else if input % 3 == 0 {
+        "Fizz".to_string()
+    } else {
+        input.to_string()
+    };
+
+    Ok(result);
 }
 
 #[no_mangle]
 pub fn node_main(mut module: Module) -> Result<()> {
-    module.export("hello", hello)
+    module.export("fizzbuzz", fizzbuzz)
 }
